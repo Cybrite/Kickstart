cd214dd207e6b68aad09dab093841fa8a9fe37ec
Harsh <harshtanishq2002@gmail.com>
2024-07-25 11:56:01 +0530

    feat: added deployment code

--- ethereum/deployExample.js
+++ ethereum/deployExample.js
@@ -1,11 +1,11 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  "REPLACE_WITH_YOUR_MNEMONIC",
   // remember to change this to your own phrase!
-  'REPLACE WITH YOUR INFURA URL'
+  "REPLACE WITH YOUR INFURA URL"
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
@@ -13,11 +13,13 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
 
   console.log(interface);
   console.log("Contract deployed to", result.options.address);
